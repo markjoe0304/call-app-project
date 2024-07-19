06a453bde230a0dabbc5f2f86adf98e4a5253b69
markjoe0304 <markjoegicheru@gmail.com>
2024-07-19 12:29:25 +0300

    moded

--- index.js
+++ index.js
@@ -29,28 +29,28 @@ function showData(data) {
 	result.innerHTML = `
         <ul class="songs">
             ${data.data
-							.map(
-								(song) => `
+					.map(
+						(song) => `
                 <li>
                     <span><strong>${song.artist.name}</strong> - ${song.title}</span>
                     <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
                 </li>`
-							)
-							.join('')}
+						)
+					.join('')}
         </ul>
     `;
 
 	if (data.prev || data.next) {
 		more.innerHTML = `
             ${
-							data.prev
-								? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-								: ''
+				data.prev
+				? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+						: ''
 						}
             ${
-							data.next
-								? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-								: ''
+				data.next
+				? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+					: ''
 						}
         `;
 	} else {
